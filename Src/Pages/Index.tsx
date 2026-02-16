8dc57d572779e94164f2bfa17e85bb3c1ed6a768
chavemestra35-wq <chavemestra35@gmail.com>
2026-02-16 04:40:21 -0300

    Add files via upload

---
+++ Src/Pages/Index.tsx
@@ -0,0 +1,21 @@
+import Hero from "@/components/Hero";
+import Services from "@/components/Services";
+import Differentials from "@/components/Differentials";
+import UrgencyCTA from "@/components/UrgencyCTA";
+import Footer from "@/components/Footer";
+import WhatsAppFloat from "@/components/WhatsAppFloat";
+
+const Index = () => {
+  return (
+    <main>
+      <Hero />
+      <Services />
+      <Differentials />
+      <UrgencyCTA />
+      <Footer />
+      <WhatsAppFloat />
+    </main>
+  );
+};
+
+export default Index;
